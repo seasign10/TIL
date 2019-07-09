80e5284dad4a597c5d7d9d794a4d3d6dada7a342
seasign10 <seasign10@gmail.com>
2019-07-09 15:47:29 +0900

    add .gitignore

---
+++ 00_startcamp/02_day/f_spring.py
@@ -0,0 +1,17 @@
+# name = '이해인'
+# print(f'안녕하세요, {name}입니다.')
+
+# 점심 메뉴 추천
+# import random
+# menu = ['초밥', '돈까스', '양고기']
+# lunch = random.choice(menu)
+# print(f'오늘의 저녁은 {lunch}입니다.')
+
+# 로또 추천
+# import random
+# numbers = range(1, 46)
+# lotto = random.sample(numbers, 6)
+# print(f'오늘의 당첨 번호는 {lotto}입니다.')
+
+name = '이해인'
+print(f'안녕하세요,' + name + '입니다.')
---
+++ 00_startcamp/02_day/number.py
@@ -0,0 +1,9 @@
+#과거
+#'%s %s' % ('one', 'two')
+
+# # pyfotmat(~3.5)
+#'{}, {}, {}'.format('one', 'two')
+#f-string(ne in 3.6)
+a = 'one'
+b = 'two'
+print(f'{a}, {b}')
