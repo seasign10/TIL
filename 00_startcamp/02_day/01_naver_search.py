7dd72a89322c128b87ce494db394b15945bf68f5
seasign10 <seasign10@gmail.com>
2019-07-09 12:11:41 +0900

    second commit

--- 00_startcamp/02_day/01_naver_search.py
+++ 00_startcamp/02_day/01_naver_search.py
@@ -7,3 +7,5 @@ soup = BeautifulSoup(html, 'html.parser')
 search = soup.select('#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div > ul > li > a .ah_k')
 for i in search:
     print(i.text)
+
+#두버내 커밋을 위한 주석!!
